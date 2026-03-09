aedd630581a565499d84631889d8718abffd63d1
engyourbae <komlaps2a@gmail.com>
2026-03-09 11:51:23 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finance-Pay-v27.4'; // Nama cache khusus aplikasi finance
+const CACHE_NAME = 'finance-Pay-v27.5'; // Nama cache khusus aplikasi finance
 const urlsToCache = [
   './',
   './index.html',
